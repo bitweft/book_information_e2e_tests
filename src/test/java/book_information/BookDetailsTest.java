508824ff8835eb985e3fb471f110c59459064b70
bitweft <bitweft@gmail.com>
2021-04-27 09:25:47 +0800

    Revert to older test name

--- src/test/java/book_information/BookDetailsTest.java
+++ src/test/java/book_information/BookDetailsTest.java
@@ -8,7 +8,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 public class BookDetailsTest extends BaseTest {
     @Test
-    public void verifyBookSearchResultsIsNotEmpty() {
+    public void verifyBookSearchResults() {
         String bookName = "harry";
         BookDetailsPage bookDetailsPage = new HomePage()
                 .searchBook(bookName)
